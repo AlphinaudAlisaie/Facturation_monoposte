a84c1f9693192da86e8b5d901c80a6c394b92ab5
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-15 10:32:29 -0400

    ajout constructeur et attributs

--- src/main/java/org/TopVentes/model/Transaction.java
+++ src/main/java/org/TopVentes/model/Transaction.java
@@ -4,14 +4,31 @@ public class Transaction {
     public final static double TAUX_POURCENTAGE_TPS = 0.05;
     public final static double TAUX_POURCENTAGE_TVQ = 0.09975;
 
-    private enum modePaiement
+    public enum MODESPAIEMENT
     {
         VISA,
-        Comptant,
-        Debit
+        COMPTANT,
+        DEBIT
     }
 
-    private int identifiantModePaiemenet;
+    private int numeroServantIdentificationPourCarte;
     private double montantAvantTaxes;
+    private int numeroTransaction;
+
+    public Transaction() {
+        this.numeroTransaction++;
+
+
+    }
+
+    /**
+     * Methode pour calculer total de la facture
+     * @return  total de la facture
+     */
+    public double calculeTotalFacture(){
+        return -1;
+    }
+
+
 
 }
