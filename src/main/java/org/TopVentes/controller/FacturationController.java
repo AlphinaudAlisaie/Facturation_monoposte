2d495a90a491e5d5d5e7fc9bbd68fda3086dcc90
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-15 10:33:17 -0400

    ajout contenu dans controller

--- src/main/java/org/TopVentes/controller/FacturationController.java
+++ src/main/java/org/TopVentes/controller/FacturationController.java
@@ -1,23 +1,33 @@
 package org.TopVentes.controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+import org.TopVentes.model.Facturation;
 
 import java.awt.desktop.AppEvent;
 
 public class FacturationController extends Application {
-    /**
-     * Methode pour lancer l'application
-     * @param primaryStage the primary stage for this application, onto which
-     *                     the application scene can be set.
-     *                     Applications may create other stages, if needed, but they will not be
-     *                     primary stages.
-     * @throws Exception
-     */
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Facturation");
+       // Facturation facturation1 = new Facturation();
 
+        TabPane root = new TabPane();
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Facturation.fxml"));
+
+        Tab tabOfFacture1 = new Tab();
+        tabOfFacture1.setText("Facture 1");
+        tabOfFacture1.setContent(fxmlLoader.load());
+
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
