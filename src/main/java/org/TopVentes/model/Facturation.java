32742a2aeb1e54448b121a8537279676dd0f58a7
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-15 10:32:54 -0400

    ajout constructeur et attributs Facturation.java

--- src/main/java/org/TopVentes/model/Facturation.java
+++ src/main/java/org/TopVentes/model/Facturation.java
@@ -5,5 +5,12 @@ public final class Facturation {
     public Vendeur vendeur;
     public Acheteur acheteur;
     public Transaction transaction;
+    private int numeroFacture;
 
+    public Facturation(Vendeur vendeur, Acheteur acheteur, Transaction transaction) {
+        this.vendeur = vendeur;
+        this.acheteur = acheteur;
+        this.transaction = transaction;
+        numeroFacture++;
+    }
 }
