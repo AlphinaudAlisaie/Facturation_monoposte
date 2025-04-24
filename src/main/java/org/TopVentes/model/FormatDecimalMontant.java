42db3d13566b54edd0734c419f221a5e9f989f30
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-24 11:23:14 -0400

    update formatDecimalMontant.java
    update interface

--- src/main/java/org/TopVentes/model/FormatDecimalMontant.java
+++ src/main/java/org/TopVentes/model/FormatDecimalMontant.java
@@ -3,6 +3,7 @@ package org.TopVentes.model;
 import java.text.DecimalFormat;
 
 public interface FormatDecimalMontant {
+    double determineNombreChiffreApresVirgules(String pMontant);
     boolean verifierStringIsDouble(String pEntree);
 
 }
