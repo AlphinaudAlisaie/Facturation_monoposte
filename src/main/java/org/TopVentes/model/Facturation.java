6936cfd9ba992d6cc38626d63440c089f37d1d78
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-24 08:37:38 -0400

    refactoring de variable transsaction pour transaction

--- src/main/java/org/TopVentes/model/Facturation.java
+++ src/main/java/org/TopVentes/model/Facturation.java
@@ -3,13 +3,13 @@ package org.TopVentes.model;
 public final class Facturation {
     public Vendeur vendeur;
     public Acheteur acheteur;
-    public Transaction transsaction;
+    public Transaction transaction;
     public int numeroFacture;
 
     public Facturation(Vendeur pVendeur, Acheteur pAcheteur, Transaction pTransaction) {
         this.vendeur = pVendeur;
         this.acheteur = pAcheteur;
-        this.transsaction = pTransaction;
+        this.transaction = pTransaction;
         numeroFacture++;
     }
 
