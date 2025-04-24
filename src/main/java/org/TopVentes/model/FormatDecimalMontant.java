c76f91a2afaa731fd2c1f2fc5a98ac1ede7e1295
Naomi Tremblay <1239654@cegeplimoilou.ca>
2025-04-24 11:22:51 -0400

    ajout methode verifierStringisDouble
    FormatMontant.java ajout verifierStringIsDouble

--- src/main/java/org/TopVentes/model/FormatDecimalMontant.java
+++ src/main/java/org/TopVentes/model/FormatDecimalMontant.java
@@ -3,6 +3,6 @@ package org.TopVentes.model;
 import java.text.DecimalFormat;
 
 public interface FormatDecimalMontant {
-    double determineNombreChiffreApresVirgules(double pMontant);
+    boolean verifierStringIsDouble(String pEntree);
 
 }
--- src/main/java/org/TopVentes/model/FormatMontant.java
+++ src/main/java/org/TopVentes/model/FormatMontant.java
@@ -8,17 +8,37 @@ import java.text.NumberFormat;
 public class FormatMontant implements FormatDecimalMontant {
 
     /**
+     * Methode pour determiner le format d'un montant
+     *
      * @param pMontant à respecter le format
-     * @return
+     * @return format correctement
      */
     @Override
-    public double determineNombreChiffreApresVirgules(double pMontant) {
-        NumberFormat formatMontant = NumberFormat.getCompactNumberInstance();
-        formatMontant.setMaximumFractionDigits(2);
-        formatMontant.setRoundingMode(RoundingMode.DOWN);
-
-        String truncatedMontant = formatMontant.format(pMontant);
+    public double determineNombreChiffreApresVirgules(String pMontant) {
+        if(verifierStringIsDouble(pMontant))
 
         return -1;
     }
+
+    /**
+     * Methode pour vérifier si le String est un double
+     * @param pEntree par comptable
+     * @return vrai si le string en param est un double
+     */
+    @Override
+    public boolean verifierStringIsDouble(String pEntree){
+        boolean isDouble= false;
+        try{
+            Double doubleNumber = Double.valueOf(pEntree);
+            isDouble = true;
+        }catch (NumberFormatException e){
+            System.out.println("L'entree en String n'est pas un nombre double!");
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return isDouble;
+    }
+
+
+
 }
